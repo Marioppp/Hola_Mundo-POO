3c5fa2bbe23464e31d499cf234ed74b98b446444
Mario Ivan Mendez Alvarez <124391477+Marioppp@users.noreply.github.com>
2023-02-18 21:29:08 -0800

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,54 @@
+
+package eva1_4_television;
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+       Television tv1 = new Television();
+        tv1.cambiarPower();
+        //ctrl + shift. Para duplicar instrucción.
+        tv1.cambiarPower();
+        tv1.cambiarPower();
+
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.subirVolumen();
+        tv1.bajarVolumen();
+        tv1.bajarVolumen();
+        tv1.bajarVolumen();         
+        tv1.bajarVolumen();         
+        tv1.bajarVolumen();         
+        tv1.bajarVolumen();         
+        tv1.bajarVolumen();         
+        tv1.bajarVolumen();         
+
+        /*tv1.subirCanal();
+        tv1.subirCanal();
+        tv1.subirCanal();
+        tv1.subirCanal();*/
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+    }
+
+}
+    
+    
+
---
+++ EVA1_4_TELEVISION/src/eva1_4_television/Television.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva1_4_television;
+
+public class Television {
+    private boolean power;
+    private int volumen;
+    private int canal;
+
+    //Métodos: Comportamientos.
+    //Constructor:
+    public Television() {  //Constructor default.
+        power = false; //Tv apagada
+        volumen = 0;
+        canal = 2;
+    }
+
+    //La regla para el nombre de los métodos debe ser un verbo.
+    public void cambiarPower() {
+        if (power == false) { //apagado
+            power = true;
+            System.out.println("TELEVISION ENCENDIDA");
+        } else {
+            power = false;
+            System.out.println("TELEVISION APAGADA");
+        }
+    }
+
+    public void subirVolumen() {
+        if (power == true) {
+            if (volumen < 100) {
+                volumen = volumen + 5;
+                System.out.println("________");
+                System.out.println("Volumen: " + volumen);
+            }
+        }
+    }
+
+    public void bajarVolumen() {
+        if (power == true) {
+            if (volumen > 0) {
+                volumen = volumen - 5;
+                System.out.println("________");
+                System.out.println("Volumen: " + volumen);
+            }
+        }
+    }
+
+    public void subirCanal() {
+        if (power == true) {
+            if (canal < 100) {
+                canal = canal + 1;
+            } else {
+                canal = 0;
+            }
+
+            System.out.println("________");
+            System.out.println("Canal: " + canal);
+        }
+    }
+
+    public void bajarCanal() {
+        if (power == true) {
+            if (canal > 0) {
+                canal = canal - 1;
+            } else {
+                canal = 100;
+            }
+
+            System.out.println("________");
+            System.out.println("Canal: " + canal);
+        }
+    }
+
+
+}
